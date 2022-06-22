dffc5a93d5c39684c41e37bfbb2640359955789c
hellodajana <dajanasmolyakov@gmail.com>
2022-06-22 14:43:11 +0200

    Separated two functions

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const switchButton = document.getElementById("switch");
 
-const monthlyPrice = document.querySelectorAll(".monthly-price");
-const annualPrice = document.querySelectorAll(".annual-price");
-
 switchButton.addEventListener("change", () => {
-  for (i = 0; i < annualPrice.length; i++) {
-    if (annualPrice[i].style.display === "none") {
-      annualPrice[i].style.display = "block";
-    } else {
-      annualPrice[i].style.display = "none";
-    }
-  }
-  for (i = 0; i < monthlyPrice.length; i++) {
-    if (monthlyPrice[i].style.display === "block") {
-      monthlyPrice[i].style.display = "none";
-    } else {
-      monthlyPrice[i].style.display = "block";
-    }
-  }
+  annual();
+  monthly();
 });
+
+function annual() {
+  const annualPrice = document.querySelectorAll(".annual-price");
+  for (i = 0; i < annualPrice.length; i++) {
+    annualPrice[i].style.display === "none"
+      ? (annualPrice[i].style.display = "block")
+      : (annualPrice[i].style.display = "none");
+  }
+}
+
+function monthly() {
+  const monthlyPrice = document.querySelectorAll(".monthly-price");
+  for (i = 0; i < monthlyPrice.length; i++) {
+    monthlyPrice[i].style.display === "block"
+      ? (monthlyPrice[i].style.display = "none")
+      : (monthlyPrice[i].style.display = "block");
+  }
+}
